9513d9e4c8ced4a81bba9c311e2157bbb90aa396
fahizp <fahizp3@gmail.com>
2023-11-26 19:48:43 +0530

    fix the cors issue

--- server/index.js
+++ server/index.js
@@ -2,20 +2,24 @@ import express from "express";
 import cors from "cors";
 import modelRoutes from './routes/modelRoutes.js';
 
-
 const app = express();
+
 app.use(cors({
-    origin:["https://coderythm.fahiz.in/"],
-    methods:["POST", "GET" ],
-    credentials:true,
+    origin: ["https://coderythm.fahiz.in"],
+    methods: ["POST", "GET"],
+    credentials: true,
 }));
-app.use(express.json({limit:'50mb'}));
+
+app.use(express.json({ limit: '50mb' }));
 app.use('/api/v1', modelRoutes);
-app.get('/', async (req,res) => {res.json('Hello From CodeRythm Test')});
+
+app.get('/', async (req, res) => {
+    res.json('Hello From CodeRythm Test');
+});
 
 const startServer = async () => {
     try {
-        app.listen(8080, () => console.log("Server has started on port http://localhost:8080"))
+        app.listen(8080, () => console.log("Server has started on port http://localhost:8080"));
     } catch (error) {
         console.log(error);
     }
